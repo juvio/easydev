864a89b4f825231eb3bbc93c5d27fdeaa2487e8d
juliana <julianavieirao.ppg@live.com>
2021-02-26 23:39:18 -0300

    corrige erro de import

--- src/components/CompletedChallenge.tsx
+++ src/components/CompletedChallenge.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
-import styles from "../styles/components/completedChallenge.module.css";
+import styles from "../styles/components/CompletedChallenge.module.css";
 
 export function CompletedChallenge(){
     const { challengesCompleted } = useContext(ChallengesContext)
